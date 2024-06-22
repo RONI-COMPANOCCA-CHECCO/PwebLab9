7f189db78dac5c93a2d728c8f020ff11cf1435b0
rcompanocca <ronicompanocca@gmail.com>
2024-06-22 15:41:50 -0500

    defini los metadatos para un componente específico.

--- lab9/src/app/ahorcado/ahorcado.component.spec.ts
+++ lab9/src/app/ahorcado/ahorcado.component.spec.ts
@@ -1,23 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit } from '@angular/core';
 
-import { AhorcadoComponent } from './ahorcado.component';
+@Component({
+  selector: 'app-ahorcado',
+  templateUrl: './ahorcado.component.html',
+  styleUrls: ['./ahorcado.component.css']
+})
 
-describe('AhorcadoComponent', () => {
-  let component: AhorcadoComponent;
-  let fixture: ComponentFixture<AhorcadoComponent>;
-
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AhorcadoComponent]
-    })
-    .compileComponents();
-
-    fixture = TestBed.createComponent(AhorcadoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
