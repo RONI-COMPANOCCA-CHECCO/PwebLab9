a039c56013296e841105e496ee0b78cb695528bb
rcompanocca <ronicompanocca@gmail.com>
2024-06-22 15:47:13 -0500

    en el componente gestionamos la selección aleatoria de palabras, la inicialización del juego y tambien vemos donde se almacenará el estado del juego como por ejm la palabra secreta, letras adivinadas, intentos restantes.

--- lab9/src/app/ahorcado/ahorcado.component.spec.ts
+++ lab9/src/app/ahorcado/ahorcado.component.spec.ts
@@ -6,3 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ahorcado.component.css']
 })
 
+export class AhorcadoComponent implements OnInit {
+  palabrasAAdivinar: string[] = ['ANGULAR', 'JAVASCRIPT', 'HTML', 'CSS', 'PROGRAMACION'];
+  palabraSecreta: string = '';
+  letrasAdivinadas: string[] = [];
+  intentosRestantes: number = 6; 
+
+  ngOnInit(): void {
+    this.iniciarJuego();
+  }
+
+  iniciarJuego(): void {
+    const randomIndex = Math.floor(Math.random() * this.palabrasAAdivinar.length);
+    this.palabraSecreta = this.palabrasAAdivinar[randomIndex];
+    this.letrasAdivinadas = Array(this.palabraSecreta.length).fill('_');
+    this.intentosRestantes = 6; // 
+  }
+}
