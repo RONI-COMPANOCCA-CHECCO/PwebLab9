41e359b27e762184e6baa9a78ef8d1325b0a9e9e
rcompanocca <ronicompanocca@gmail.com>
2024-06-22 21:04:32 -0500

    intento hacer la conexion con html pero no lo logro

--- lab9/src/app/ahorcado/ahorcado.component.ts
+++ lab9/src/app/ahorcado/ahorcado.component.ts
@@ -11,47 +11,39 @@ export class AhorcadoComponent {
   intentos = 0;
   gano = false;
   perdio = false;
-  letras = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-            'K', 'L', 'M', 'N', 'Ñ', 'O', 'P', 'Q', 'R', 'S',
-            'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
   constructor() {
-    this.palabraOculta = '_ '.repeat(this.palabra.length);
+    this.palabraOculta = '_'.repeat(this.palabra.length);
   }
 
   comprobar(letra: string) {
-    this.existeLetra(letra);
-
-    const palabraOcultaArr = this.palabraOculta.split(' ');
-
-    for (let i = 0; i < this.palabra.length; i++) {
-      if (this.palabra[i] === letra) {
-        palabraOcultaArr[i] = letra;
+    if (this.palabra.includes(letra)) {
+      // Actualiza palabraOculta con la letra correcta
+      let nuevaPalabraOculta = '';
+      for (let i = 0; i < this.palabra.length; i++) {
+        if (this.palabra[i] === letra) {
+          nuevaPalabraOculta += letra;
+        } else {
+          nuevaPalabraOculta += this.palabraOculta[i];
+        }
       }
+      this.palabraOculta = nuevaPalabraOculta;
+      this.verificaGane();
+    } else {
+      this.intentos++;
+      this.verificaPerdida();
     }
-
-    this.palabraOculta = palabraOcultaArr.join(' ');
-    this.verificaGane();
   }
 
   verificaGane() {
-    const palabraArr = this.palabraOculta.split(' ');
-    const palabraEvaluar = palabraArr.join('');
-
-    if (palabraEvaluar === this.palabra) {
+    if (this.palabraOculta === this.palabra) {
       this.gano = true;
-    }
-
-    if (this.intentos >= 9) {
-      this.perdio = true;
     }
   }
 
-  existeLetra(letra: string) {
-    if (this.palabra.indexOf(letra) >= 0) {
-      // console.log('La letra existe ' + letra);
-    } else {
-      this.intentos++;
+  verificaPerdida() {
+    if (this.intentos >= 9) {
+      this.perdio = true;
     }
   }
 }
